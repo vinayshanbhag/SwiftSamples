1c65291f899b87fff059e541835e7c85191ab2fb
Vinay Shanbhag <vinayshanbhag@users.noreply.github.com>
2019-11-12 14:52:35 -0600

    Update ListFilters.swift

--- ListFilters.swift
+++ ListFilters.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+// Parse a String containing a JSON list, into an array of Observation objects.
+// Filter array to retrieve objects of specific kind of Observation
+// Sort the array by observation date
+
 // add extension to parse JSON from String
 extension String {
     func parse<D>(to type: D.Type) -> D? where D: Decodable {
@@ -18,59 +22,24 @@ extension String {
 // Sample JSON - list of observations
 var jsonString = """
    [
-    {
-        "kind":"heart rate",
-        "reading":70,
-        "dtm":"2019-11-11T10:00:00Z"
-    },
-    {
-        "kind":"heart rate",
-        "reading":72,
-        "dtm":"2019-11-09T13:00:00Z"
-    },
-    {
-        "kind":"heart rate",
-        "reading":75,
-        "dtm":"2019-11-10T14:00:00Z"
-    },
-    {
-        "kind":"temperature",
-        "reading":98,
-        "dtm":"2019-11-11T10:00:00Z"
-    },
-    {
-        "kind":"temperature",
-        "reading":100,
-        "dtm":"2019-11-09T16:00:00Z"
-    },
-    {
-        "kind":"temperature",
-        "reading":99,
-        "dtm":"2019-11-10T15:00:00Z"
-    },
-    {
-        "kind":"spO2",
-        "reading":98,
-        "dtm":"2019-11-11T11:00:00Z"
-    },
-    {
-        "kind":"spO2",
-        "reading":95,
-        "dtm":"2019-11-09T09:00:00Z"
-    },
-    {
-        "kind":"spO2",
-        "reading":99,
-        "dtm":"2019-11-10T17:00:00Z"
-    }
+    { "kind":"heart rate", "value":70, "unit":"bpm", "dateObserved":"2019-11-11T10:00:00Z" },
+    { "kind":"heart rate", "value":72, "unit":"bpm", "dateObserved":"2019-11-09T13:00:00Z" },
+    { "kind":"heart rate", "value":75, "unit":"bpm", "dateObserved":"2019-11-10T14:00:00Z" },
+    { "kind":"temperature","value":98, "unit":"F",   "dateObserved":"2019-11-11T10:00:00Z" },
+    { "kind":"temperature","value":100,"unit":"F",   "dateObserved":"2019-11-09T16:00:00Z" },
+    { "kind":"temperature","value":99, "unit":"F",   "dateObserved":"2019-11-10T15:00:00Z" },
+    { "kind":"spO2",       "value":98, "unit":"%",   "dateObserved":"2019-11-11T11:00:00Z" },
+    { "kind":"spO2",       "value":95, "unit":"%",   "dateObserved":"2019-11-09T09:00:00Z" },
+    { "kind":"spO2",       "value":99, "unit":"%",   "dateObserved":"2019-11-10T17:00:00Z" }
    ]
 """
 
 // Observation type
 struct Observation: Codable {
     var kind:String
-    var reading:Int
-    var dtm:Date
+    var value:Int
+    var unit:String
+    var dateObserved:Date
 }
 
 // Parse JSON into a list of Observations
@@ -80,10 +49,10 @@ let observations = jsonString.parse(to: [Observation].self)
 let filteredObservations = observations!.filter({$0.kind=="heart rate"})
 
 // sort filtered observations by date descending
-let sortedFilteredObservations = filteredObservations.sorted(by:{$0.dtm.compare($1.dtm) == .orderedDescending})
+let sortedFilteredObservations = filteredObservations.sorted(by:{$0.dateObserved.compare($1.dateObserved) == .orderedDescending})
 
 // print
 for i in sortedFilteredObservations {
-    print("Kind:\(i.kind), Reading:\(i.reading), Date:\(i.dtm)")
+    print("Kind:\(i.kind), Value:\(i.value)\(i.unit), Date:\(i.dateObserved)")
 }
 
