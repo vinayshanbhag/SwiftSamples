5af6bf84ab44be2539ef7613eead007f96b1e674
Vinay Shanbhag <vinayshanbhag@users.noreply.github.com>
2019-11-15 07:52:19 -0600

    Update ParseJSON.swift

--- ParseJSON.swift
+++ ParseJSON.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// Parse a String containing a JSON list, into an array of Observation objects.
+// Parse a JSON response from url, into an array of Observation objects.
 // Filter array to retrieve objects of specific kind of Observation
 // Sort the array by observation date
 
@@ -19,25 +19,20 @@ extension String {
     }
 }
 
-func query(address: String) -> String {
-    let url = URL(string: address)
-    let semaphore = DispatchSemaphore(value: 0)
-    
-    var result: String = ""
-    
-    let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
-        result = String(data: data!, encoding: String.Encoding.utf8)!
-        semaphore.signal()
-    }
-    
-    task.resume()
-    semaphore.wait()
-    return result
+// extend URL to add http get request
+extension URL {
+  func get() -> String {
+      let semaphore = DispatchSemaphore(value: 0) 
+      var result: String = ""
+      let task = URLSession.shared.dataTask(with: self) {(data, response, error) in
+          result = String(data: data!, encoding: String.Encoding.utf8)!
+          semaphore.signal()
+      } 
+      task.resume()
+      semaphore.wait()
+      return result
+  }
 }
-
-// Sample JSON - list of observations
-let jsonString = query(address: "https://raw.githubusercontent.com/vinayshanbhag/SwiftSamples/master/observations.json")
-
 
 struct Observation : Codable {
     struct Quantity : Codable {
@@ -65,24 +60,32 @@ struct Observation : Codable {
     let issued:Date
 }
 
+// Utility functions to pretty print observations
+func printObs(_ obs:[Observation]) {
+  _ = obs.map(printObs)
+}
+func printObs(_ obs:Observation) {
+  print("\(obs.display) - \(obs.valueQuantity.value)\(obs.valueQuantity.unit) \(obs.method.coding[0].display) - \(obs.issued)")
+}
+
+
+// Sample url- json response with list of observations
+let url = "https://raw.githubusercontent.com/vinayshanbhag/SwiftSamples/master/observations.json"
+let jsonString = URL(string:url)!.get()
 // parse observations 
 let observations = jsonString.parse(to:[Observation].self)!
 
 // filter observations by code
 print("--filtered--")
 let filteredObservations = observations.filter({$0.code=="8480-6"})
-for obs in filteredObservations{
-    print("\(obs.display) - \(obs.valueQuantity.value)\(obs.valueQuantity.unit) \(obs.method.coding[0].display) - \(obs.issued)")
-}
+printObs(filteredObservations)
+
 
 // Sort observations by issued date
-print("--sorted--")
+print("\n\n--sorted--")
 let sortedFilteredObservations = filteredObservations.sorted(by:{$0.issued.compare($1.issued) == .orderedDescending})
-for obs in sortedFilteredObservations{
-    print("\(obs.display) - \(obs.valueQuantity.value)\(obs.valueQuantity.unit) \(obs.method.coding[0].display) - \(obs.issued)")
-}
+printObs(sortedFilteredObservations)
 
 // Latest observation
-print("--latest--")
-var obs = filteredObservations.sorted(by:{$0.issued.compare($1.issued) == .orderedDescending})[0]
-print("\(obs.display) - \(obs.valueQuantity.value)\(obs.valueQuantity.unit) \(obs.method.coding[0].display) - \(obs.issued)")
+print("\n\n--latest--")
+printObs(sortedFilteredObservations[0])
